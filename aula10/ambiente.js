65042d5c7260af05f1f1e631b7f14e4c68870313
Kaique Machado <Kaiquemachado5@users.noreply.github.com>
2025-09-30 18:11:49 -0300

    Atualizando Contador

---
+++ aula10/ambiente.js
@@ -0,0 +1,10 @@
+/* c = 1;
+
+while (c <= 10) {
+    console.log(`Vou te pegar! ${c}`);
+    c++;
+} */
+
+for (var c = 1; c <= 10; c++) {
+    console.log(`Agora veja ${c};`)
+}
---
+++ aula11/scripts/main.js
@@ -0,0 +1,37 @@
+function contar() {
+    let ini = document.querySelector('#ini');
+    let fim = document.querySelector('#fim');
+    let pas = document.querySelector('#pas');
+    let res = document.querySelector('#res');
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert('Que merda!');
+        res.innerHTML = 'Impossível contar';
+    } else {
+        res.innerHTML = 'Contando... <br>';
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(pas.value);
+        if (p <= 0) {
+            window.alert("Passo inválido! Considerando paso '1'.");
+            p = 1;
+        }
+        if (i < f){
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} &#128073; `;
+            }
+        } else {
+            // Contagem descrescente
+            for (let c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} &#128073; `;
+            }
+        }
+        res.innerHTML += '&#127770;'
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#confirmar").addEventListener("click", contar);
+});
